8999d76224731b51171ebda7416f8d64f52a9caa
Alex Waibel <al3x.waib3l@gmail.com>
2018-05-13 16:58:58 -0400

    Reworked argument parsing by implementing argparse.

--- bibtextohtml.py
+++ bibtextohtml.py
@@ -7,31 +7,26 @@ from a given BibTeX file and then store the result in a specified HTML
 output file. These files are all given as command line arguments.
 """
 
+import argparse
 import sys
 
 
-def print_help_text():
-    """Prints usage text to console."""
-    helptext = """
-    Usage: bibtextohtml [OPTION...]
-    bibtextohtml fills HTML templates with info from BibTeX files.
+def open_files():
+    """Opens files given as command line arguments."""
+    program_description = ("Fills HTML template with information parsed from "
+                           "a BibTeX file and stores the output in another HTML file.")
+    parser = argparse.ArgumentParser(description=program_description)
+    parser.add_argument('-t', metavar='template', help='template HTML file', required=True)
+    parser.add_argument('-i', metavar='inputFile', help='input BibTeX file', required=True)
+    parser.add_argument('-o', metavar='outputFile', help='output HTML file', required=True)
 
-      Example:
-       bibtextohtml -t "template.html" -i "input.bib" -o "output.html"
-
-      Options:
-       -t     template HTML file
-       -i     input BibTeX file
-       -o     output HTML file
-    """
-
-    print(helptext)
+    if len(sys.argv) != 4:
+        parser.print_help()
 
 
 def main():
-    """Read in command line arguments and perform template filling."""
-    if len(sys.argv) != 3:
-        print_help_text()
+    """Perform template filling."""
+    open_files()
 
 
 if __name__ == "__main__":
