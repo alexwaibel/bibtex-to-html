a7969f176b745b97285d4cca8cafeade2ccbacc9
Alex Waibel <al3x.waib3l@gmail.com>
2018-05-13 17:20:54 -0400

    fixed incorrect check for number of arguments

--- bibtextohtml.py
+++ bibtextohtml.py
@@ -13,6 +13,11 @@ import sys
 
 def open_files():
     """Opens files given as command line arguments."""
+
+    """We expect three actual arguments but they each have a flag and the first
+    item in argc is the program name so this totals to 7 expected items."""
+    EXPECTED_ARG_COUNT = 7
+
     program_description = ("Fills HTML template with information parsed from "
                            "a BibTeX file and stores the output in another HTML file.")
     parser = argparse.ArgumentParser(description=program_description)
@@ -20,9 +25,13 @@ def open_files():
     parser.add_argument('-i', metavar='inputFile', help='input BibTeX file', required=True)
     parser.add_argument('-o', metavar='outputFile', help='output HTML file', required=True)
 
-    if len(sys.argv) != 4:
-        parser.print_help()
+    if len(sys.argv) != EXPECTED_ARG_COUNT:
+        # Unrecoverable error if user does not supply all arguments
+        parser.print_help(sys.stderr)
+        sys.exit(1)
 
+    args = parser.parse_args()
+    print(args)
 
 def main():
     """Perform template filling."""
